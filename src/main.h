95d87f8e0d4096eafe61b0a793980d3ad420e17f
Syaiful Nizam Yahya <syaifulnizamyahya@gmail.com>
2019-03-03 20:40:39 +0800

    Renamed folder. Removed unused codes.

--- src/main.h
+++
@@ -1,9 +0,0 @@
-﻿// autogtest.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-#include "..\sqr\sqr.h"
-
-// TODO: Reference additional headers your program requires here.

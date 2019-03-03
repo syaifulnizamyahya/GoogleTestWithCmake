9b3b0d75b12f2b632718d2cb0f5ee5e355af360e
Syaiful Nizam Yahya <syaifulnizamyahya@gmail.com>
2019-03-03 20:41:35 +0800

    Added missing files

---
+++ main-project/main.cpp
@@ -0,0 +1,13 @@
+﻿// autogtest.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Square 5 = " << sqr(5) << endl;
+	cin.get();
+	return 0;
+}
---
+++ main-project/main.h
@@ -0,0 +1,9 @@
+﻿// autogtest.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "..\sqr\sqr.h"
+
+// TODO: Reference additional headers your program requires here.

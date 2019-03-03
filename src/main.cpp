c247bb4891af6b881c9fcf4a958a4b9068d1941c
Syaiful Nizam Yahya <syaifulnizamyahya@gmail.com>
2019-03-03 20:23:30 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+﻿// autogtest.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "Square 5 = " << sqr(5) << endl;
+	cin.get();
+	return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "../sqr/sqr.h"
+
+TEST(TestSuiteName, TestName)
+{
+	EXPECT_EQ(sqr(5), 25);
+}
